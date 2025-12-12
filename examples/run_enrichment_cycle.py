4a4715a87984d9f9ba6ae471a0713f36edcea115
jingnafeng <163872795+jingnafeng@users.noreply.github.com>
2025-12-12 13:47:26 -0600

    Delete examples directory

--- examples/run_enrichment_cycle.py
+++
@@ -1,6 +0,0 @@
-import os, json
-from ontocodex.core.orchestrator import OntoCodexOrchestrator
-DATA = os.environ.get("ONTOCODEX_DATA_DIR", "data")
-orch = OntoCodexOrchestrator(data_dir=DATA, llm_backend="auto")
-res = orch.run_enrichment_cycle("Enrich ontology with diabetes guideline treatments", "diabetes")
-print(json.dumps(res, indent=2))
--- examples/run_enrichment_from_user_ontology.py
+++
@@ -1,8 +0,0 @@
-import os, json
-from ontocodex.core.orchestrator import OntoCodexOrchestrator
-DATA = os.environ.get("ONTOCODEX_DATA_DIR", "data")
-USER_OWL = os.environ.get("ONTOCODEX_USER_ONTOLOGY", os.path.join(DATA, "user_ontology.ttl"))
-orch = OntoCodexOrchestrator(data_dir=DATA, llm_backend="auto")
-result = orch.run_enrichment_cycle(user_goal="Enrich user ontology using MedlinePlus then DOID", user_ontology_path=USER_OWL, requested_relations=["treated_with","has_symptom","has_lab_test"])
-print(json.dumps(result, indent=2))
-print("\nGenerated script:", result.get("enrichment_script",{}).get("script_path"))
